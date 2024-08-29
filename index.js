83cef0d3dc3ae11e882a4a2cbe339a17e7c47902
João Paulo de Araujo Tiburcio <124619394+JoaoPauloArauj@users.noreply.github.com>
2024-08-29 01:30:04 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let anoAtual= 2024;
+const nomeCompleto = "João Paulo Araujo Tiburcio";
+let idade = 17;
+const cursoFaculdade = "Engenharia Elétrica";
+let cidadeAtual = "Curitiba"
+
+
+anoAtual = 2025;
+idade = 18;
+
+console.log(`No ano que vem meu nome ira ser o mesmo, ${nomeCompleto}. Vou ter ${idade} anos, planejo estar morando em ${cidadeAtual}, quero estar cursando ${cursoFaculdade}, `);
